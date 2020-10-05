debca9aee76a3ffce397dabd7d38894552154bb5
lrny-lru <60946728+lrny-lru@users.noreply.github.com>
2020-10-05 12:23:02 -0400

    lets get this party started

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import BookSearch from './BookSearch';
+import SearchFilter from './SearchFilter';
+import BookResult from './BookResult'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render(){
+    return (
+      <div className="App">
+        <header>
+          <h1>Google Book Search</h1>
+        </header>
+        <BookSearch />
+        <SearchFilter />
+        <BookResult />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/BookResult.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class BookResult extends Component{
+    render(){
+        return(
+            <></>
+        )
+    }
+}
+
+export default BookResult;
